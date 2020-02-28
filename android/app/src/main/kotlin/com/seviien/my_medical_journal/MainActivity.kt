72848ce7f8615dc903a492592f436f7345226e96
Puay Hiang <puayhiang@garuda.io>
2020-02-28 20:49:01 +0800

    Initial Commit

---
+++ android/app/src/main/kotlin/com/seviien/my_medical_journal/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.seviien.my_medical_journal
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
