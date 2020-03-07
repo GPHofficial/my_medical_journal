a39180a2392b530d2ac36d9310d83ad2c659da06
Vivek Adrakatti <vivek.adrakatti1@gmail.com>
2020-03-07 14:56:29 +0800

    Small Changes to Medication Tracker

--- lib/entities/medication.dart
+++ lib/entities/medication.dart
@@ -7,5 +7,46 @@ class Medication {
   int _quantity;
   List<String> _specialInfo;
 
+  Medication();
+  String get medication=> _medication;
+  String get nickname=> _nickname;
+  int get dosage=> _dosage;
+  int get frequency=> _frequency;
+  int get quantity=> _quantity;
+
+  List<String> get reminders=>_reminders;
+  List<String> get specialInfo=>_specialInfo;
+
+  void setName(String medication){
+    this._medication = medication;
+  }
+  void setNickname(String medication){
+    this._nickname = medication;
+  }
+  void setDosage(int medication){
+    this._dosage = medication;
+  }
+  void setFrequency(int medication){
+    this._frequency = medication;
+  }
+  void setQuantity(int medication){
+    this._quantity= medication;
+  }
+  void setReminders(List<String> medication){
+    this._reminders = medication;
+  }
+  void setSpecialInfo(List<String> medication){
+    this._specialInfo = medication;
+  }
+
+  void disp(){
+    print("Medication Name: "+_medication);
+    print("Medication NickName: "+_nickname);
+    print("Medication Dosage: "+ _dosage.toString());
+    print("Medication Frequency: "+_frequency.toString());
+    print("Medication Quantity: "+_quantity.toString());
+  }
+
+
 
 }
