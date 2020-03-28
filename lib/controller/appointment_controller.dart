9f0de6025c32faa6339bc992406f1577df38f24f
keekong <44111167+keekong@users.noreply.github.com>
2020-03-28 14:36:04 +0800

    Add files via upload

---
+++ lib/controller/appointment_controller.dart
@@ -0,0 +1,61 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:my_medical_journal/adapters/firestore.dart';
+import 'package:my_medical_journal/entities/appointment.dart';
+
+class AppointmentController{
+
+  FirestoreAdapter<Appointment> firestore;
+  static List<Function> observers;
+
+  AppointmentController(){
+    firestore = new FirestoreAdapter<Appointment>(Appointment.collectionName);
+    observers = new List<Function>();
+    listenObservers();
+  }
+
+  void addAppointment(Appointment Appointment){
+    firestore.createDocument(Appointment);
+  }
+
+  Future<Appointment> getAppointment(String generatedId) async{
+    Map<String,dynamic> appointmentMap = (await firestore.getDocument(generatedId));
+    Appointment appointment = Appointment.castFromMap(appointmentMap);
+    return appointment;
+  }
+
+  Future<Appointment> editAppointment(Appointment updateAppointment) async{
+    Map<String,dynamic> appointmentMap = (await firestore.updateDocument(updateAppointment));
+    Appointment appointment = Appointment.castFromMap(appointmentMap);
+    return appointment;
+  }
+
+  Future<List<Appointment>> listAppointment() async{
+    return castAppointmentList(await firestore.listDocument());
+  }
+
+  void addAppointmentObserver(Function func){
+    observers.add(func);
+  }
+
+  void listenObservers(){
+    firestore.listenCollection((QuerySnapshot qs){
+      AppointmentController.callAppointmentObserver(qs);
+    });
+  }
+
+  static List<Appointment> castAppointmentList(List<Map<String,dynamic>> appointmentMap){
+    List<Appointment> appointmentList = new List<Appointment>();
+    for(var appointment in appointmentMap){
+      appointmentList.add(Appointment.castFromMap(appointment));
+    }
+    return appointmentList;
+  }
+
+  static void callAppointmentObserver(QuerySnapshot qs){
+    List<Map<String,dynamic>> snapshotList = FirestoreAdapter.castSnapshotList(qs.documents);
+    List<Appointment> appointmentList = castAppointmentList(snapshotList);
+    for(var func in AppointmentController.observers){
+      func(appointmentList);
+    }
+  }
+}
